b2c7735f58f35e0a96e0bb2806739cd3fe0a2d65
Felipe <47871564+felipemarkson@users.noreply.github.com>
2022-03-26 13:27:13 -0300

    Final Version

--- src/DERs.jl
+++ src/DERs.jl
@@ -24,51 +24,65 @@ function configure_limits!(MW::Vector{Float64}, MVAr::Vector{Float64}, configs::
 end
 
 function dg(configs::Config, bus)
-    MW = [0.0, configs.power]
-    MVAr = [-configs.power, configs.power]
+    power = configs.power #if configs.α < 1.0  configs.power/(1 - configs.α) else  configs.power end
+    MW = [0.0, power]
+    MVAr = [-power, power]
 
     MW, MVAr = configure_limits!(MW, MVAr, configs)
 
+    if isapprox(configs.α, 1.0; atol=1e-3)
+        oper = [0.0]
+    else
+        oper = [0.0 , 1]
+    end
+
     return DistSystem.DER(
         bus,                  # Bus
-        configs.power,                # Sᴰᴱᴿ (MVA)
+        power,                # Sᴰᴱᴿ (MVA)
         configs.energy,                # Eᴰᴱᴿ (MWh)
         configs.α,                # αᴰᴱᴿ
         configs.β,                # βᴰᴱᴿ
         MW,         # [Pᴰᴱᴿ_low, Pᴰᴱᴿ_upper]  (MW)
         MVAr,       # [Qᴰᴱᴿ_low, Qᴰᴱᴿ_upper]  (MVAr)
-        [0.0, 1.0],          # Possible DER's Operation Scenarios ≠ μᴰᴱᴿ
+        oper,          # Possible DER's Operation Scenarios ≠ μᴰᴱᴿ
         [0.0] # Costs (Not used) 
     )
 end
 
 function ess(configs::Config, bus)
-    MW = [-configs.power, configs.power]
-    MVAr = [-configs.power, configs.power]
+    power = configs.power #if configs.α < 1.0  configs.power/(1 - configs.α) else  configs.power end
+    MW = [0.0, power]
+    MVAr = [-power, power]
 
     MW, MVAr = configure_limits!(MW, MVAr, configs)
 
+    if isapprox(configs.α, 1.0; atol=1e-3)
+        oper = [0.0]
+    else
+        oper = [-1.0, 0.0, 1.0]
+    end
+
     return DistSystem.DER(
         bus,                  # Bus
-        configs.power,                # Sᴰᴱᴿ (MVA)
+        power,                # Sᴰᴱᴿ (MVA)
         configs.energy,                # Eᴰᴱᴿ (MWh)
         configs.α,                # αᴰᴱᴿ
         configs.β,                # βᴰᴱᴿ
         MW,         # [Pᴰᴱᴿ_low, Pᴰᴱᴿ_upper]  (MW)
         MVAr,       # [Qᴰᴱᴿ_low, Qᴰᴱᴿ_upper]  (MVAr)
-        [-1.0, 0.0, 1.0],          # Possible DER's Operation Scenarios ≠ μᴰᴱᴿ
+        oper,          # Possible DER's Operation Scenarios ≠ μᴰᴱᴿ
         [0.0] # Costs (Not used) 
     )
 end
 
 function ev_charger(configs::Config, bus)
-    MW = [-configs.power, 0.0]
-    MVAr = [-configs.power, configs.power]
-
+    power = configs.power #if configs.α < 1.0  configs.power/(1 - configs.α) else  configs.power end
+    MW = [0.0, power]
+    MVAr = [-power, power]
     MW, MVAr = configure_limits!(MW, MVAr, configs)
     return DistSystem.DER(
         bus,                  # Bus
-        configs.power,                # Sᴰᴱᴿ (MVA)
+        power,                # Sᴰᴱᴿ (MVA)
         configs.energy,                # Eᴰᴱᴿ (MWh)
         configs.α,                # αᴰᴱᴿ
         configs.β,                # βᴰᴱᴿ
