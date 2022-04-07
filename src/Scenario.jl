9b6e7a1a0134d2cd3124c568d8e8c0d98f753bb4
Felipe <47871564+felipemarkson@users.noreply.github.com>
2022-04-06 23:03:49 -0300

    Change the scenarios and fix the main

--- src/Scenario.jl
+++ src/Scenario.jl
@@ -2,41 +2,43 @@ module Scenario
 include("./DERs.jl")
 import .DERs
 
+export *
+
 function renewable(sys, alpha)
-    renewable_location = [25, 22]
-    renewable_config = DERs.Config(false, true, 0.05, 0.0, alpha, 0.0)
+    renewable_location = [26, 29, 13, 6]
+    renewable_config = DERs.Config(false, true, 0.1, 0.0, alpha, 0.0)
     for location in renewable_location
         push!(sys.dgs, DERs.dg(renewable_config, location))
     end
     return sys
 end
 function ess(sys, alpha)
-    ess_location = [10, 20]
-    ess_config = DERs.Config(true, true, 0.05, 0.05, alpha, alpha)
+    ess_location = [4, 32]
+    ess_config = DERs.Config(true, true, 0.1, 0.05, alpha, alpha)
     for location in ess_location
         push!(sys.dgs, DERs.ess(ess_config, location))
     end
     return sys
 end
 function ev(sys, alpha)
-    ev_location = [5, 23]
-    ev_config = DERs.Config(false, true, 0.05, 0.0, alpha, 0.0)
+    ev_location = [17, 20]
+    ev_config = DERs.Config(false, true, 0.1, 0.0, alpha, 0.0)
     for location in ev_location
         push!(sys.dgs, DERs.ev_charger(ev_config, location))
     end
     return sys
 end
 function dg_dispached(sys, alpha)
-    dg_dispached_location = [18, 33]
-    dg_dispached_config = DERs.Config(true, true, 0.05, 100.0, 1.0, 1.0)
+    dg_dispached_location = [18, 33, 22, 25]
+    dg_dispached_config = DERs.Config(true, true, 0.2, 100.0, 1.0, 1.0)
     for location in dg_dispached_location
         push!(sys.dgs, DERs.dg(dg_dispached_config, location))
     end
     return sys
 end
 function ess_dispached(sys, alpha)
-    ess_dispached_location = [12, 22]
-    ess_dispached_config = DERs.Config(true, true, 0.05, 0.1, 1.0, 1.0)
+    ess_dispached_location = [9]
+    ess_dispached_config = DERs.Config(true, true, 0.1, 0.1, 1.0, 1.0)
     for location in ess_dispached_location
         push!(sys.dgs, DERs.ess(ess_dispached_config, location))
     end
